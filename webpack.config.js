76f44b0825f50cbdbf59a49d6918392760cacc96
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2025-12-03 18:17:20 +0100

    reconfigure webpack to handle multiple css files

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+   entry: {
+    index: "./src/main.js",
+    page: "./src/script.js",
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    static: './dist'
+    static: "./dist",
   },
-  
+
   plugins: [
     new CleanWebpackPlugin(),
     new ESLintPlugin({
       context: "compiler.context",
       eslintPath: "eslint",
-      extensions: 'js',
-      exclude: 'node_modules',
+      extensions: "js",
+      exclude: "node_modules",
       fix: false,
-      formatter: 'stylish',
+      formatter: "stylish",
       lintDirtyModulesOnly: false,
       threads: false,
       emitError: true,
@@ -31,23 +35,39 @@ module.exports = {
       failOnError: true,
       failOnWarning: false,
       quiet: false,
-      outputReport: false
+      outputReport: false,
     }),
+
+    
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: 'Project Name',
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["index"], // ← Only include index bundle + CSS
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/page.html",
+      filename: "page.html",
+      chunks: ["page"], // ← Only include page bundle + CSS
+    }),
+
+
   ],
   module: {
     rules: [
-      {
+       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
